0e7bddddf86a0dd99c45e212c2a36f35b21cc144
Berke Kesgin <kesginberke9@gmail.com>
2024-09-12 10:52:24 +0300

    Initial Commit

---
+++ uyuSleep/uyuSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  uyuSleepApp.swift
+//  uyuSleep
+//
+//  Created by Berke Kesgin on 12.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct uyuSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
